035e6940a602c6bdcb3c36baccbcee87d430cb43
Reed Wilson <reedcwilson@gmail.com>
2017-01-16 14:37:11 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Madame Zamad - A lesson on lists
+ 
+For an explanation of the assignment and requirements go to the [Programming
+Fundamentals](https://programming.reedcwilson.com/madame-zamad.html) website.
+
---
+++ madame-zamad.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# import your modules here
+
+# create a list of mystic responses
+# create a list of wait responses
+# create a list of fortunes
+
+# ask for a yes/no question or a fortune
+
+# do the mystic response if they wanted a question
+# do the fortune if they asked for a fortune
